249a7fad6c98b0895e5dea27a78d4df3edd62f7e
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2022-06-24 01:54:49 +0600

    Cryptoverse init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import {Link} from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOulined } from '@ant-design/icons';
+export default function NavBar() {
+  return (
+    <div className='nav-container'>
+
+        <div className='logo-container'>
+
+        </div>
+        
+    </div>
+  )
+}
