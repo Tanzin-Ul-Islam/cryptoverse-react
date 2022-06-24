8f4fdd925982bea95919f39fbcee2f7c10b45b51
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2022-06-25 02:03:10 +0600

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOulined } from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from "../assets/images/cryptocurrency.png"
 export default function NavBar() {
     return (
